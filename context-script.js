a98766d7fbc723e68fccfc1e46adf197a6a743d4
Daniel Tillett <daniel@nucleics.com>
2017-01-06 15:55:48 +1100

    Update to 1.03
    Just a few small changes to support the update to 1.03 on the Chrome
    Webstore.

--- context-script.js
+++ context-script.js
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2014 Junling Guo
+    Copyright (c) 2016 Junling Guo & Daniel Tillett
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2014 Daniel Tillett & Junling Guo
+    Copyright (c) 2016 Daniel Tillett & Junling Guo
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
